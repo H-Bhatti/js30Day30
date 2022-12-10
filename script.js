be48a758a664f2fc99e5e26013c924f4ef4108c4
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2022-12-10 14:55:32 +0400

    done also added speed features

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let previousHole;
+let score = 0 ;
+let timeUp = false;
+
+
+function randomTime (min, max){
+    return Math.round(Math.random()*(max-min)+min);
+
+}
+
+function randomMole (holes) {
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index]
+    if (hole == previousHole){
+        console.log("Wrong hole!! 0w0");
+        return randomMole(holes);
+    }
+    previousHole = hole;
+    return hole;
+}
+
+function pepPop(min, max){
+    const time = randomTime(min, max);
+    const hole = randomMole(holes);
+    // console.log (time,hole)
+    hole.classList.add("up");
+    setTimeout(()=>{
+        hole.classList.remove("up")
+        if(!timeUp) pepPop(min, max);
+    },time)
+}
+
+function startGame(min, max){
+    scoreBoard.textContent = 0;
+    timeUp= false;
+    score = 0;
+    pepPop(min, max)
+    setTimeout(()=>{
+        timeUp=true
+    }, 10000)
+}
+
+function bonk(e){
+    if(!e.isTrusted) return;
+    score++ 
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(element => element.addEventListener("click",bonk))
